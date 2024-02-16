fd75f5918f542a80a657d2eedc6f217c5190898d
alelomelir <alejandralomeli2712@gmail.com>
2024-02-16 14:32:10 -0600

    Delete for

--- Diagnostico.java
+++ Diagnostico.java
@@ -7,9 +7,7 @@ public class Diagnostico {
             System.out.println("Veces que corrio el primer for: " + i);
             for (int j = 0; j <= 3; j++) {
                 System.out.println("Veces que corrio el segundo for: " + j);
-                for (int k = 0; k <= 3; k++) {
-                    System.out.println("Veces que corrio el tercer for: " + k);
-                }
+               
             }
         }
     }
