4dd9b0609e145590d615e26754ab3fdcc65402eb
alelomelir <alejandralomeli2712@gmail.com>
2024-02-19 12:15:15 -0600

    Change the array to bidimensional

--- Diagnostico.java
+++ Diagnostico.java
@@ -1,12 +1,18 @@
-
 public class Diagnostico {
 
     public static void main(String[] args) {
         int i = 0;
-        int[] arreglo = { 1, 2, 3, 4, 5, 6 };
+        int j = 0;
+        int[][] arreglo = {
+                { 1, 2, 3, 4, 5, 6 },
+                { 7, 8, 9, 10, 11, 12 }
+        };
 
-        for (i = 0; i <= arreglo.length; i++) {
-            System.out.println(arreglo[i]);
+        for (i = 0; i <= 2; i++) { // itera entre arreglos
+            for (j = 0; j <= 5; j++) { // itera entre elementos
+                System.out.println(arreglo[i][j]);
+            }
+
         }
     }
 }
