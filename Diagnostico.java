321bb82add7b08649a52ca53f175d26e7cd1e0fd
alelomelir <alejandralomeli2712@gmail.com>
2024-02-19 13:44:15 -0600

    Add two bidimensional array & adds them

--- Diagnostico.java
+++ Diagnostico.java
@@ -3,16 +3,43 @@ public class Diagnostico {
     public static void main(String[] args) {
         int i = 0;
         int j = 0;
-        int[][] arreglo = {
-                { 1, 2, 3, 4, 5, 6 },
-                { 7, 8, 9, 10, 11, 12 }
+        int[][] arreglo1 = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+
+        int[][] arreglo2 = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
 
         for (i = 0; i <= 2; i++) { // itera entre arreglos
-            for (j = 0; j <= 5; j++) { // itera entre elementos
-                System.out.println(arreglo[i][j]);
-            }
+            for (j = 0; j <= 2; j++) { // itera entre elementos
 
+                System.out.println(arreglo1[i][j]);
+            }
+        }
+
+        for (i = 0; i <= 2; i++) {
+            for (j = 0; j <= 2; j++) {
+
+                System.out.println(arreglo2[i][j]);
+            }
+        }
+
+        int[][] arreglo3 = new int[2][2];
+        for (i = 0; i <= 2; i++) {
+            for (j = 0; j <= 2; j++) {
+                System.out.println(arreglo1[i][j] + arreglo2[i][j]); // i y j para que sea dinamico
+                arreglo3[i][j] = arreglo1[i][j] + arreglo2[i][j];
+            }
+        }
+        for (i = 0; i <= 2; i++) {
+            for (j = 0; j <= 2; j++) {
+
+            }
         }
     }
 }
